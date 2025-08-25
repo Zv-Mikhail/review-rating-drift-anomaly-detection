a241b343c5a2812438cf091ce59924909ebcb16d
Mishnya <mishnya@Mac-mini-Mishnya.local>
2025-08-25 21:32:17 +0300

    Update

--- parser/core/utils/webdriver.py
+++ parser/core/utils/webdriver.py
@@ -1,18 +1,25 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 class WebDriverManager:
-    """Класс для управления WebDriver."""
-
     @staticmethod
-    def create_webdriver():
-        options = webdriver.ChromeOptions()
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_argument("--start-maximized")
-        options.add_argument("--disable-direct-composition")
-        options.add_argument("--incognito")
-        return webdriver.Chrome(
-            service=Service(ChromeDriverManager().install()), options=options
-        )
+    def create_webdriver(headless: bool = False):
+        opts = ChromeOptions()
+        if headless:
+            opts.add_argument("--headless=new")
+        # стабильность и меньше «первого запуска»
+        opts.add_argument("--disable-blink-features=AutomationControlled")
+        opts.add_argument("--disable-dev-shm-usage")
+        opts.add_argument("--no-sandbox")
+        opts.add_argument("--disable-gpu")
+        opts.add_argument("--incognito")
+        opts.add_argument("--no-first-run")
+        opts.add_argument("--no-default-browser-check")
+        opts.add_argument("--disable-extensions")
+        opts.add_experimental_option("excludeSwitches", ["enable-automation"])
+        opts.add_experimental_option("useAutomationExtension", False)
+
+        driver = webdriver.Chrome(options=opts)
+        driver.set_page_load_timeout(60)
+        driver.implicitly_wait(8)   # мягкий дефолт, но будем использовать явные ожидания
+        return driver
