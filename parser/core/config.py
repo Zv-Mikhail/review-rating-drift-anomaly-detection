3d7b23e7c0985d0df29d0f67725980779cfd21be
Звягинцев Михаил <126711879+Zv-Mikhail@users.noreply.github.com>
2025-08-28 15:41:58 +0300

    Update config.py

--- parser/core/config.py
+++ parser/core/config.py
@@ -1,5 +1,5 @@
 BASE_URL = "https://www.wildberries.ru/catalog/"
-WAIT_TIMEOUT = 50
+WAIT_TIMEOUT = 13
 RESULTS_DIR = "parser/results"
 FILENAME_TEMPLATE = "wb_reviews_{}_{}.xlsx"
-WAIT_TIMEOUT_GET_PRODUCT = 10
+WAIT_TIMEOUT_GET_PRODUCT = 7
